1400b444f2431053fa437e67938ba818d121392e
Dubsta <boller.dominik@gmail.com>
2025-08-30 11:17:31 +0200

    conditional lora

--- __init__.py
+++ __init__.py
@@ -1,17 +1,23 @@
 from .nodes.load_lora_url_node import LoadLoraByUrlOrPath
 from .nodes.load_video_lora_url_node import LoadVideoLoraByUrlOrPath, LoadVideoLoraByUrlOrPathSelect
-from .nodes.load_nunchaku_lora_url_node import LoadNunchakuLoraFromUrlOrPath
 
 NODE_CLASS_MAPPINGS = {
     "LoadLoraFromUrlOrPath": LoadLoraByUrlOrPath,
     "LoadVideoLoraFromUrlOrPath": LoadVideoLoraByUrlOrPath,
     "LoadVideoLoraFromUrlOrPathSelect": LoadVideoLoraByUrlOrPathSelect,
-    "LoadNunchakuLoraFromUrlOrPath": LoadNunchakuLoraFromUrlOrPath
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "LoadLoraFromUrlOrPath": "Load LoRA (URL/Path) to Stack",
     "LoadVideoLoraFromUrlOrPath": "Load Video LoRA (URL/Path) & Apply",
     "LoadVideoLoraFromUrlOrPathSelect": "Load Video LoRA (URL/Path) Wan",
-    "LoadNunchakuLoraFromUrlOrPath": "Nunchaku FLUX LoRA Loader (URL/Path)"
 }
+
+# Conditionally load nunchaku node if module is available
+try:
+    import nunchaku
+    from .nodes.load_nunchaku_lora_url_node import LoadNunchakuLoraFromUrlOrPath
+    NODE_CLASS_MAPPINGS["LoadNunchakuLoraFromUrlOrPath"] = LoadNunchakuLoraFromUrlOrPath
+    NODE_DISPLAY_NAME_MAPPINGS["LoadNunchakuLoraFromUrlOrPath"] = "Nunchaku FLUX LoRA Loader (URL/Path)"
+except ImportError:
+    pass
